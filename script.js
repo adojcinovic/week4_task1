bd1671e7ef5c7d4ffe9fb8f6d565eb3bf722145b
adojcinovic <adojca@gmail.com>
2021-09-27 21:07:53 +0200

    js functionality add

---
+++ script.js
@@ -0,0 +1,8 @@
+var email = document.getElementById('email')
+var message = document.getElementById('message')
+var button = document.getElementById('button')
+var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+button.addEventListener('click', () => {
+    !emailRegex.test(email.value) ? message.classList.add('message_show') : message.classList.remove('message_show') && message.classList.add('message')
+})
