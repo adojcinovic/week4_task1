b5ed67304faf652b08dc61b47183f3c6fb6569fe
adojcinovic <adojca@gmail.com>
2021-09-30 10:19:41 +0200

    requested changes

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 button.addEventListener('click', () => {
     !emailRegex.test(email.value) ? message.classList.add('message_show') : message.classList.remove('message_show') && message.classList.add('message')
+    email.placeholder = 'example@quantox.com'
 })
